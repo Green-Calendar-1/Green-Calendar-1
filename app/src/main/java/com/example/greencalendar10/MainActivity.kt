35bf7ec539470bbdaa3f51ca7c953c79066881de
hyunjo01 <hyunjo10217@gmail.com>
2022-08-14 11:28:49 +0900

    8/14 툴바 임시 틀

--- app/src/main/java/com/example/greencalendar10/MainActivity.kt
+++ app/src/main/java/com/example/greencalendar10/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.greencalendar10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import com.example.greencalendar10.databinding.ActivityLoginBinding
+import com.example.greencalendar10.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         // 하단 바 바인딩
         val bnv_main = findViewById(R.id.bottomNavigationView) as BottomNavigationView
@@ -37,5 +43,24 @@ class MainActivity : AppCompatActivity() {
         }
             selectedItemId = R.id.homeFragment
         }
+
+        // toolbar
+        val toolbar: Toolbar = findViewById(R.id.main_toolbar)
+        toolbar.inflateMenu(R.menu.main_toolbar_menu)
+
+        toolbar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.action_notification -> {
+                    val intentToNotification = Intent(this, NotificationList::class.java)
+                    startActivity(intentToNotification)
+                }
+            }
+            true
+        }
     }
+
+
+
+
+
 }
